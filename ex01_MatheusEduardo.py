817c8961d51f197dc67203975ba1de628d198769
MatheusESouza21 <bmakgacha@gmail.com>
2025-02-17 22:09:09 -0300

    ex01

---
+++ ex01_MatheusEduardo.py
@@ -0,0 +1,23 @@
+#Escreva um programa que forneça um tipo de aplicação financeira adequado a um investidor a partir de 2 dados fornecidos: O grau de aceitação de risco e o valor a ser investido.
+#Deve ser lido no teclado na forma BX ou AL risco. Se for fornecido algo diferente disso, o programa deve mostrar uma mensagem indicando que foi fornecido um dado inválido.
+#Para o valor, deve ser um número real.
+risco = input('Digite o grau de risco (BX ou AL): ').casefold()
+if risco != 'BX' or 'AL':
+    print('Risco invalido!')
+
+
+
+investimento = float(input('Insira o investimento desejado: '))
+
+if investimento < 1000 and risco == 'BX':
+    print('Recomendamos que invista em poupança.')
+
+elif investimento >= 1000 and risco == 'BX':
+    print('Recomendamos que invista em renda fixa.')
+
+elif investimento < 1000 and risco == 'AL':
+    print('Recomendamos que invista em bitcoins.')
+
+elif investimento >= 1000 and risco == 'AL':
+    print('Recomendamos que invista em ações.')
+
